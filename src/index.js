ac8e1f31b12e4b540660fecc906f3d5c743409f2
Walcimar Zanque Denadai <walcimar@outlook.com>
2025-05-11 15:27:04 -0300

    projeto concluido

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 let nomeHeroi = "Goku";
 let nivelHeroi = 5001;
 let nivelDescricao = "";
@@ -33,14 +34,3 @@ switch (true) {
 
 console.log(`O Herói de nome **${nomeHeroi}** está no nível de **${nivelDescricao}**!`);
 
-
-
-
-//Se XP for menor do que 1.000 = Ferro
-//Se XP for entre 1.001 e 2.000 = Bronze
-//Se XP for entre 2.001 e 5.000 = Prata
-//Se XP for entre 5.001 e 7.000 = Ouro
-//Se XP for entre 7.001 e 8.000 = Platina
-//Se XP for entre 8.001 e 9.000 = Ascendente
-//Se XP for entre 9.001 e 10.000= Imortal
-//Se XP for maior ou igual a 10.001 = Radiante
